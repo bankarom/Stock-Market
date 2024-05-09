4d8fa7559140c3f96ba9798195d904e54f8ee0fe
bankarom <ombankar5678@gmail.com>
2024-05-09 13:31:21 +0530

    schema +

--- clients/Stock/src/App.jsx
+++ clients/Stock/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Signup from './Signup';
 import Login from './Login';
+
 import './App.css'; 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
+        
       </Routes>
     </BrowserRouter>
   );
--- clients/Stock/src/Home.jsx
+++ clients/Stock/src/Home.jsx
@@ -11,6 +11,9 @@ function Home() {
         <Link to="/signup" className="button">Sign Up</Link>
         <Link to="/login" className="button">Login</Link>
       </div>
+     
+      
+     
     </div>
   );
 }
