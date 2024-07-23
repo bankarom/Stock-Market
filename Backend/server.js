e5224309c812261187799b26ea6eb04083e3b7e6
bankarom <ombankar5678@gmail.com>
2024-07-23 15:19:47 +0530

    new-get

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,28 @@ app.get('/healthcheck',(req,res)=>{
 app.use(express.json())
 let connectionStatus = 'disconnected';
 
- 
+const startDatabase = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://Admin:Admin@cluster0.lm8nxzv.mongodb.net/stock-market?retryWrites=true&w=majority&appName=Cluster0');
+        connectionStatus = "The database has been connected!!";
+    } catch (err) {
+        console.error("Failed to connect to database");
+        connectionStatus = "Failed to connect to database";
+        console.log('error',err)
+    }
+};
+
+app.get('/data',async(req,res)=>{
+    try{
+        const data = await User.find();
+        console.log(data);
+        res.json(data)
+    }
+    catch(err){
+        res.status(500).send(err)
+        console.log(err);
+    }
+})  
 app.get('/',(req,res)=>{
     res.send(connectionStatus)
 })
