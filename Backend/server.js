85deb6b21b21b79d91264f506ab42cd2803300e5
bankarom <ombankar5678@gmail.com>
2024-07-24 13:59:39 +0530

    op

--- Backend/server.js
+++ Backend/server.js
@@ -1,46 +1,34 @@
-const express = require('express')
-const app = express()
-const {User} = require("./schema")
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const { User } = require('./schema');
+const mongoose = require('mongoose');
 
+app.get('/healthcheck', (req, res) => {
+    res.send('pong');
+});
 
-app.get('/healthcheck',(req,res)=>{
-    res.send('pong')
-})
-
-app.use(express.json())
+app.use(express.json());
 let connectionStatus = 'disconnected';
 
 const startDatabase = async () => {
     try {
         await mongoose.connect('mongodb+srv://Admin:Admin@cluster0.lm8nxzv.mongodb.net/stock-market?retryWrites=true&w=majority&appName=Cluster0');
-        connectionStatus = "The database has been connected!!";
+        connectionStatus = 'The database has been connected!!';
     } catch (err) {
-        console.error("Failed to connect to database");
-        connectionStatus = "Failed to connect to database";
-        console.log('error',err)
+        console.error('Failed to connect to database');
+        connectionStatus = 'Failed to connect to database';
+        console.log('error', err);
     }
 };
 
-app.get('/data',async(req,res)=>{
-    try{
-        const data = await User.find();
-        console.log(data);
-        res.json(data)
-    }
-    catch(err){
-        res.status(500).send(err)
-        console.log(err);
-    }
-})  
-app.get('/',(req,res)=>{
-    res.send(connectionStatus)
-})
 
-app.listen(3000,()=>{
-    startDatabase()
-    console.log('success')
-})
+app.get('/', (req, res) => {
+    res.send(connectionStatus);
+});
 
+app.listen(3000, () => {
+    startDatabase();
+    console.log('Server is running on port 3000');
+});
 
-module.exports = app
+module.exports = app;
